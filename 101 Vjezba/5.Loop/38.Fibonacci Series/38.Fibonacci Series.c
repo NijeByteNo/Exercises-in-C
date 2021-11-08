b2cc8e392b82b6a305e50db762a5acb7444149bf
NijeByteNo <93339252+NijeByteNo@users.noreply.github.com>
2021-11-08 20:50:31 +0100

    Update 38.Fibonacci Series.c
    Samo sam dodao */ na kraju jer nisam zatvorio komentar. PRovjeravam kako radi GIT hub desktop

--- 101 Vjezba/5.Loop/38.Fibonacci Series/38.Fibonacci Series.c
+++ 101 Vjezba/5.Loop/38.Fibonacci Series/38.Fibonacci Series.c
@@ -15,3 +15,12 @@ int main(void)
     
     
 }
+
+/*Ufff, Ovo sam nasao na internetu jer sam se previse iznervirao i nisam mogao rijesit, evo i sada nakon par prelazaka preko koda
+nisam siguran da sam skuzio logiku... 
+Prvo se printa first pa onda se mijenja u temp, pa first postaje second pa ima formula ova dolje...
+Kako sam shvatio u drugom krugu, first je sada second, sto znaci printa se second i dalje kako idemo krugom sada 
+second(njegova trenutna vrijednost) postaje temp,preskace se "first = second", pa onda opet formula 
+pa u trecem krugu isto sve, preskace se "first = second". MA NEMAM POJMA...
+
+NAsao sam objasnjenje ovdje https://www.youtube.com/watch?v=5DhDhTO_ALw */
